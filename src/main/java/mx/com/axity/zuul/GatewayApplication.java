0c6873f5734094e0dc0e2c5f4912fa81689f07ce
Javier Rodriguez <javierrodriguez@MacBook-Pro-de-usuario.local>
2018-07-18 14:05:41 -0500

    Se integran zuul y eureka

--- src/main/java/mx/com/axity/zuul/GatewayApplication.java
+++ src/main/java/mx/com/axity/zuul/GatewayApplication.java
@@ -4,9 +4,11 @@ import mx.com.axity.zuul.filters.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+@EnableEurekaServer
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
